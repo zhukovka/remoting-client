1b7b359b7f2aeab7d9cc10a7feff367df9861b21
abush <abush@PUNKOR-PC>
2018-01-23 22:42:48 +0200

    remoting client

--- RemotingOneClient1/Program.cs
+++ RemotingOneClient1/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using RemotingOne;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,24 @@ namespace RemotingOneClient1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press return after the server is started");
+            Console.ReadLine();
+
+            ChannelServices.RegisterChannel(new TcpClientChannel(), true);
+            Hello obj = (Hello)Activator.GetObject(
+                                         typeof(Hello), "tcp://localhost:8086/Hi");
+            if (obj == null)
+            {
+                Console.WriteLine("could not locate server");
+                return;
+            }
+            Console.WriteLine(obj.Greeting("Helen Zhukova"));
+            MySerialized ser = obj.GetMySerialized();
+            if (!RemotingServices.IsTransparentProxy(ser))
+            {
+                Console.WriteLine("ser is not a transparent proxy");
+            }
+            ser.Foo();
         }
     }
 }
